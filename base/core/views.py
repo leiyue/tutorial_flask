7cdcd432be73c032955958bd33f9f87ff6956c44
leiyue <mr.leiyue@gmail.com>
2016-02-05 00:10:04 +0800

    fixing nav login form bug

--- base/core/views.py
+++ base/core/views.py
@@ -6,11 +6,15 @@
 from flask import Blueprint, render_template
 from flask_menu import register_menu
 
+from base.auth.forms import LoginForm
+
 core = Blueprint('urls', __name__, template_folder='templates')
 
 
 @core.route('/')
 @register_menu(core, '.main.home', u'首页', order=0)
 def index():
+    form = LoginForm()
     return render_template('core/index.html',
-                           title=u'首页')
+                           title=u'首页',
+                           form=form)
